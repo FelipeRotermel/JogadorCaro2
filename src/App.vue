a3f4f0fb84d75186bab24d9759a8973e60e6adeb
Paulo <pauloramosj213@gmail.com>
2022-05-30 15:11:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>Cabeçalho</header>
+  <main>Main</main>
+  <footer>Footer</footer>
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+  font-weight: normal;
+  height: 100vh;
+}
+
+header, footer {
+  background-color: rgb(100,50,150);
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+
+main {
+  height: 70%;
+}
+
+</style>
